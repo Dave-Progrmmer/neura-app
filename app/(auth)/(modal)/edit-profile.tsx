706d004c5925cc07f0d76729e5291dccc9ee3433
David <david@example.com>
2025-10-24 01:02:50 +0100

    Refactor edit profile modal to improve image upload functionality; enhance user experience with updated profile picture handling and maintain existing user data updates. Update API definitions to include messaging and push modules.

--- app/(auth)/(modal)/edit-profile.tsx
+++ app/(auth)/(modal)/edit-profile.tsx
@@ -1,60 +1,85 @@
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '@/constants/Colors';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
-import { Id } from '@/convex/_generated/dataModel';
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
-import * as Sentry from '@sentry/react-native';
+import { Id } from '@/convex/_generated/dataModel';
 import * as ImagePicker from 'expo-image-picker';
-import { Colors } from '@/constants/Colors';
-const editProfile = () => {
-    const { biostring, linkstring, userId, imageUrl } = useLocalSearchParams<{
-        biostring: string;
-        linkstring: string;
-        userId: string;
-        imageUrl: string;
-      }>();
-      const [bio, setBio] = useState(biostring);
-      const [link, setLink] = useState(linkstring);
-      const updateUser = useMutation(api.users.updateUser);
-      const generateUploadUrl = useMutation(api.users.generateUploadUrl);
-      const updateImage = useMutation(api.users.updateImage);
-      const [selectedImage, setSelectedImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
-  
-    
-      const router = useRouter();
-      const selectImage = async () => {
-        const result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          allowsEditing: true,
-          aspect: [4, 3],
-        });
-        if (!result.canceled) {
-          setSelectedImage(result.assets[0]);
-        }
-      };
-      const onDone = async () => {
-        updateUser({ _id: userId as Id<'users'>, bio, websiteUrl: link });
-        Sentry.captureEvent({
-          message: 'User Profile updated',
-          extra: {
-            bio,
-            link,
-          },
-        }); 
+import * as Sentry from '@sentry/react-native';
 
-        router.dismiss()
-      };
+const Page = () => {
+  const { biostring, linkstring, userId, imageUrl } = useLocalSearchParams<{
+    biostring: string;
+    linkstring: string;
+    userId: string;
+    imageUrl: string;
+  }>();
 
-    
+  const [bio, setBio] = useState(biostring);
+  const [link, setLink] = useState(linkstring);
+  const updateUser = useMutation(api.users.updateUser);
+  const generateUploadUrl = useMutation(api.users.generateUploadUrl);
+  const updateImage = useMutation(api.users.updateImage);
+
+  const [selectedImage, setSelectedImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
+
+  const router = useRouter();
+
+  const onDone = async () => {
+    updateUser({ _id: userId as Id<'users'>, bio, websiteUrl: link });
+    Sentry.captureEvent({
+      message: 'User Profile updated',
+      extra: {
+        bio,
+        link,
+      },
+    });
+    if (selectedImage) {
+      await updateProfilePicture();
+    }
+    router.dismiss();
+  };
+
+  const updateProfilePicture = async () => {
+    // Step 1: Get a short-lived upload URL
+    const postUrl = await generateUploadUrl();
+
+    // Convert URI to blob
+    const response = await fetch(selectedImage!.uri);
+    const blob = await response.blob();
+
+    // Step 2: POST the file to the URL
+    const result = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': selectedImage!.mimeType! },
+      body: blob,
+    });
+    const { storageId } = await result.json();
+    console.log('🚀 ~ updateProfilePicture ~ storageId:', storageId);
+    // Step 3: Save the newly allocated storage id to the database
+    await updateImage({ storageId, _id: userId as Id<'users'> });
+  };
+
+  const selectImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0]);
+    }
+  };
+
   return (
     <View>
-     <Stack.Screen
+      <Stack.Screen
         options={{
           headerRight: () => (
             <TouchableOpacity onPress={onDone}>
               <Text style={styles.doneButtonText}>Done</Text>
-            </TouchableOpacity> 
+            </TouchableOpacity>
           ),
         }}
       />
@@ -65,7 +90,7 @@ const editProfile = () => {
           <Image source={{ uri: imageUrl }} style={styles.image} />
         )}
       </TouchableOpacity>
-       <View style={styles.section}>
+      <View style={styles.section}>
         <Text style={styles.label}>Bio</Text>
         <TextInput
           value={bio}
@@ -82,37 +107,35 @@ const editProfile = () => {
         <TextInput value={link} onChangeText={setLink} placeholder="Link" autoCapitalize="none" />
       </View>
     </View>
-  )
-}
-
-export default editProfile
-
+  );
+};
+export default Page;
 const styles = StyleSheet.create({
-    section: {
-      marginBottom: 16,
-      borderWidth: 1,
-      borderColor: Colors.border,
-      borderRadius: 4,
-      padding: 8,
-      margin: 16,
-    },
-    bioInput: {
-      height: 100,
-    },
-    label: {
-      fontSize: 14,
-      fontWeight: 'bold',
-      marginBottom: 4,
-    },
-    doneButtonText: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      color: Colors.submit,
-    },
-    image: {
-      width: 100,
-      height: 100,
-      borderRadius: 50,
-      alignSelf: 'center',
-    },
-  });
+  section: {
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: Colors.border,
+    borderRadius: 4,
+    padding: 8,
+    margin: 16,
+  },
+  bioInput: {
+    height: 100,
+  },
+  label: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  doneButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.submit,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: 'center',
+  },
+});
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,8 @@
  */
 
 import type * as http from "../http.js";
+import type * as messages from "../messages.js";
+import type * as push from "../push.js";
 import type * as users from "../users.js";
 
 import type {
@@ -27,6 +29,8 @@ import type {
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  messages: typeof messages;
+  push: typeof push;
   users: typeof users;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
